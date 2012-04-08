e41c4a389578de0bef819f9f94e17d753de554b4
Mikhail Sobolev <mss@mawhrin.net>
2012-04-08 21:11:17 +0300

    add possibility to write to files

--- src/esums_file.erl
+++ src/esums_file.erl
@@ -1,10 +1,23 @@
 -module(esums_file).
 
 -export([
-    calc/1
+    calc/1,
+    open/1,
+    write/2,
+    complete/2,
+    complete/1,
+    info/1
 ]).
 
 -define(BLOCK_SIZE, 128*1024).
+
+-record(esums_file, {
+    name :: binary(),
+    mtime :: integer(),
+    fd :: term(),
+    completed = false :: boolean(),
+    sums :: term()
+}).
 
 calc(FileName) ->
     {ok, Device} = file:open(FileName, [read, raw, binary]),
@@ -20,3 +33,42 @@ read_all(Device, BlockSize, Context) ->
         eof ->
             esums:final(Context)
     end.
+
+open(Name) when is_binary(Name) ->
+    case file:open(Name, [write, raw, delayed_write]) of
+        {ok, FD} ->
+            #esums_file{
+                name = Name,
+                mtime = esums_helpers:utc(),
+                fd = FD,
+                completed = false,
+                sums = esums:new()
+            };
+
+        Other ->
+            Other
+    end;
+open(Name) when is_list(Name) ->
+    open(list_to_binary(Name)).
+
+write(#esums_file{fd=FD, completed=false, sums=Sums}=State, Buffer) when is_binary(Buffer) ->
+    case file:write(FD, Buffer) of
+        ok ->
+            State#esums_file{
+                mtime = esums_helpers:utc(),
+                fd = FD,
+                sums = esums:update(Sums, Buffer)
+            };
+
+        Other ->
+            Other
+    end.
+
+complete(State, Buffer) ->
+    complete(write(State, Buffer)).
+
+complete(#esums_file{completed=false, sums=Sums}=State) ->
+    State#esums_file{mtime = esums_helpers:utc(), completed=true, sums=esums:final(Sums)}.
+
+info(#esums_file{name=Name, mtime=MTime, completed=true, sums=Sums}) ->
+    {Name, MTime, Sums}.
